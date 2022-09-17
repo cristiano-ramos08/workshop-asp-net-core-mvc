99ae64ed0dac17ee8733e2d4128c5881e6a361b4
Cristiano Ramos <limaramos.c@gmail.com>
2022-09-17 19:01:52 -0300

    Validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -28,7 +28,7 @@ namespace SalesWebMvc.Controllers
         }
 
         public IActionResult Create()
-        {
+        {            
             var departments = _departmentService.FindAll();
             var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
@@ -38,7 +38,13 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
-             _sellerService.Insert(seller);
+            if (!ModelState.IsValid)
+            {
+                var departiments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departiments };
+                return View(viewModel);
+            }
+            _sellerService.Insert(seller);
 
             return RedirectToAction(nameof(Index));
         }
@@ -102,6 +108,13 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departiments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departiments };
+                return View(viewModel);
+            }
+            
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
