b6be55de12ceac8cf3303ad6f1c452e9f6710f4c
Cristiano Ramos <limaramos.c@gmail.com>
2022-09-19 16:34:29 -0300

    Update to .net Core 3.1 version

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -79,13 +79,16 @@ namespace SalesWebMvc
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
